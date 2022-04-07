4c39caae55c77834056552fc71563728a947b88e
Kevv-thon <kevinbramastaa@gmail.com>
2022-04-07 07:33:31 +0700

    first first

---
+++ app/src/main/java/com/example/myrecyclerview/Hero.java
@@ -0,0 +1,31 @@
+package com.example.myrecyclerview;
+
+public class Hero {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    private String name;
+    private String from;
+    private int photo;
+}
